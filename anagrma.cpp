7b7910cb698013f5321e9bde145e3c9ffa4685a5
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-09-04 11:16:55 +0200

    4

---
+++ anagrma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool check(string s1, string s2)
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}
---
+++ reszta.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T[9] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int change, amount, i;
+
+    cout << "enter the change: ";
+    cin >> change;
+    i = 0;
+
+    while (change > 0)
+    {
+        if(change > T[i])
+        {
+            amount = change / T[i];
+            change -= T[i] * amount;
+            cout << T[i] << "*" << amount << "\n";
+        }
+        i++;
+    }
+
+    return 0;
+}
