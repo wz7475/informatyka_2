dd8ac858844f327c14d24901474a0fa86c25ae25
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-09-25 11:17:35 +0200

    update

--- python/antypierwsze.py
+++ python/antypierwsze.py
@@ -5,3 +5,19 @@ def amount_factors(x):
             factors.append(i)
     return len(factors)
 
+def antifirst(x):
+    for i in range(1, x):
+        if amount_factors(i) >= amount_factors(x):
+            return False
+    return True
+
+n = int(input("enter number:\n"))
+suma = 0
+
+for i in range(1, n +1):
+    if antifirst(i):
+        suma += i
+        print(i)
+
+#print(suma)
+#print(antifirst(n))
